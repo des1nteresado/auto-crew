25f3aa2a7d128dd4f3bd142b4414a46f9851e66d
Daniil Chernyshev <des1nteresado@outlook.com>
2021-01-23 21:49:30 +0300

    added spec for CommonColorPicker

---
+++ frontend/src/tests/common/components/CommonColorPicker.spec.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import CommonColorPicker from 'common/components/CommonColorPicker';
+import { useNakedComponent } from 'helpers/hooks/useNakedComponent';
+
+const defaultProps = {
+  value: 'value',
+  selectedValue: 'value',
+};
+
+const setUp = (props) => shallow(<CommonColorPicker {...defaultProps} {...props} />);
+
+describe('CommonColorPicker', () => {
+  let component;
+
+  beforeEach(() => {
+    component = setUp();
+  });
+
+  it('renders CommonColorPicker component correctly', () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it('sets checked prop to true when selectedValue equals value', () => {
+    const colorPicker = mount(<CommonColorPicker {...defaultProps} />);
+    const input = colorPicker.find('input');
+
+    expect(input.props().checked).toBeTruthy();
+  });
+});

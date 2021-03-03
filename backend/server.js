c32a6dd096568ee982df93e0d2b3841179e5db81
Daniil Chernyshev <des1nteresado@outlook.com>
2021-03-04 00:23:06 +0300

    Moved main router from server.js; Updated structure;

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,15 @@
 import express from "express";
 
 import config from "./config";
+import router from "./router";
 
 import "./db";
 
 const app = express();
-const router = express.Router();
 
 app.use(express.json());
 
-router.get("/ping", (req, res) => {
-    res.json({ message: "pong" });
-});
-
-app.use(router);
+app.use(config.API_PREFIX, router);
 
 app.listen(config.PORT, () => {
     console.log(`> Ready on http://localhost:${config.PORT}`);

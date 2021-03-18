f7a5ac6ab5ce35cc592d7ad455576b7521ef43d1
Daniil Chernyshev <des1nteresado@outlook.com>
2021-03-19 00:54:22 +0300

    Added prod config

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv';
 
 import development from './development';
+import production from './production';
 
 dotenv.config();
 
 const config = {
   development,
+  production,
 };
 
 export default config[process.env.NODE_ENV || 'development'];

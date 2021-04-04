004599caec27975db92efe1fe9a834d8b486d959
Daniil Chernyshev <des1nteresado@outlook.com>
2021-04-04 14:38:10 +0300

    Updated getUser endpoint

--- backend/src/modules/user/controllers/getUser.js
+++ backend/src/modules/user/controllers/getUser.js
@@ -13,9 +13,7 @@ module.exports = async (req, res) => {
         return res.status(404).json({ message: 'User does not exist.' });
       }
 
-      return res.status(200).json({
-        ...user.toObject(),
-      });
+      return res.status(200).json(user);
     }
     return res.status(400).json({ message: 'Provided userId is no valid.' });
   } catch (error) {
